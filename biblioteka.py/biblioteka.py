4fd64b4597610b8339f0532a2ec8333b4bc51ef7
Justyna Nowakowska <nowakowska.just@gmail.com>
2020-06-23 15:41:49 +0200

    Search list

--- biblioteka.py/biblioteka.py
+++ biblioteka.py/biblioteka.py
@@ -16,27 +16,29 @@ class FilmSeriesList:
             print(i)
 
     def get_movies(self):
-        filmy = [i for i in self.full_list if str(type(i)) == "<class '__main__.Movie'>"]
-
+        output_list = []
         for i in self.full_list:
-            if isinstance(i, Series):
-                self.series_list.append(i)
-                self.text_series_list.append(str(i))
+            if isinstance(i, Serial):
+                pass
             else:
-                self.movie_list.append(i)
-                self.text_movie_list.append(str(i))
+                output_list.append(i)
+        return output_list
 
-        self.text_series_list.sort()
-        self.text_movie_list.sort()
-        print(self.text_series_list)
-        print(self.text_movie_list)
+    def get_series(self):
+        output_list = []
+        for i in self.full_list:
+            if isinstance(i, Serial):
+                output_list.append(i)
+        return output_list      
 
     def search(self, title):
-        return f"FilmSeriesList(title={self.title})"
-    
-    print(search)
+        movies = []
+        for movie in self.full_list:
+            if movie.title == title:
+                movies.append(movie)
+        return movies
 
-class Movies: 
+class Movie: 
     def __init__ (self, title, year, species, plays):
         self.title = title
         self.year = year
@@ -46,17 +48,15 @@ class Movies:
     def __str__ (self):
         return f"{self.title} ({self.year})"
 
-    # def __repr__(self):
-    #     return f"Movies(title={self.title}, year={self.year}, species={self.species}, plays={self.plays})"
-
     def play(self, step = 1):
         self.plays += step
 
-film1 = Movies(title = "Szeregowiec Ryan", year = "1998", species = "war drama", plays=0)
-film2 = Movies(title = "Pierwszy człowiek", year = "2018", species = "biographical", plays=0)
+film1 = Movie(title = "Saving Private Ryan", year = "1998", species = "war drama", plays=0)
+film2 = Movie(title = "First Man", year = "2018", species = "biographical", plays=0)
+film3 = Movie(title = "The Green Mile", year = "1999", species = "drama", plays = 0)
 
 
-class Series(Movies):
+class Serial(Movie):
     def __init__(self, serie, episode, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.serie = serie
@@ -65,18 +65,19 @@ class Series(Movies):
     def __str__(self):
         return f"{self.title} S{self.serie}E{self.episode}"
 
-    # def __repr__(self):
-    #     return f"Series(serie={self.serie}, episode{self.episode})"
-
-serie1 = (Series(title = "Greys Anatomy", year = "2005", species = "comedy-drama", serie = "01", episode = "15", plays=0))
-serie2 = Series(title = "Breaking Bad", year = "2008", species = "comedy", serie = "05", episode = "17", plays=0)
-
+serie1 = Serial(title = "Greys Anatomy", year = "2005", species = "comedy-drama", serie = "01", episode = "15", plays=0)
+serie2 = Serial(title = "Breaking Bad", year = "2008", species = "comedy", serie = "05", episode = "17", plays=0)
+serie3 = Serial(title = "La Casa Del Papel", year = "2017", species = "thriller", serie = "03", episode = "07", plays = 0)
 
 addition=FilmSeriesList()
 addition.add(film1)
 addition.add(film2)
+addition.add(film3)
 addition.add(serie1)
 addition.add(serie2)
+addition.add(serie3)
+
+print(addition.search("Greys Anatomy"))
 
 
 
